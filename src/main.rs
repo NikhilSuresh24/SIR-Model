2907344b79506d22071450e01e9b2ab967a7b87b
Eliot Cowan <elicowa@nus11754-11-elicowa.nueva.local>
2019-03-19 09:31:08 -0700

    Fixed a few bugs

--- src/main.rs
+++ src/main.rs
@@ -12,7 +12,7 @@ fn main() {
     let mut s: Vec<f32> = vec![(params.total_population * (1.0 - I_POP_PERCENT)).round()];
     let mut i: Vec<f32> = vec![(params.total_population * I_POP_PERCENT).round()];
     let mut r: Vec<f32> = vec![R_START_POP];
-    let mut d: Vec<f32> = vec![0];
+    let mut d: Vec<f32> = vec![0.0];
     for _iter in 1..NUM_STEPS + 1 {
         step(&mut s, &mut i, &mut r, &mut d, params.beta, params.gamma, params.dt, params.alpha);
     }
@@ -24,7 +24,7 @@ fn step(s: &mut Vec<f32>, i: &mut Vec<f32>, r: &mut Vec<f32>, d: &mut Vec<f32>, 
     s.push((s[last_idx] - beta * dt * s[last_idx] * i[last_idx]));
     i.push((i[last_idx] + (beta * s[last_idx] - gamma) * i[last_idx] * dt)-i[last_idx]*alpha*dt);
     r.push((r[last_idx] + gamma * i[last_idx] * dt));
-    d.push((d[last_idx] +r[last_idx]*alpha*dt);
+    d.push(d[last_idx] +r[last_idx]*alpha*dt);
 }
 
 struct Params {
